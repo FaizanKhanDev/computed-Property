d67f9a7dcad33db01be9e1326f5ad8f07182556d
FaizanKhanDev <faizan480khan@gmail.com>
2023-03-17 12:25:28 +0500

    first Commit

---
+++ app/components/Forloop.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div v-for="item in items"> 
+            <h1>{{ item.message }}</h1>
+        </div>
+        <p>De structure</p>
+        <li v-for="(item,index) in items">{{ parentMessage }}  {{ index }} {{ item.message }}  </li> <br /> <br />
+        <li v-for="(value,key,index) in myObjects"> {{ index }} {{ key }}: {{ value }}</li>
+        
+    </div>
+</template>
+<script>
+    export default {
+        name:'ForLoop',
+        data(){
+            return{
+                parentMessage:'Parent Message',
+                items:[
+                    {
+                        message:'foo',
+
+                    },
+                    {
+                        message:'bar'
+                    }
+                ],
+                myObjects:{
+                    title:'how to do lists in Vue',
+                    author: 'Jane Doe',
+                    publishedAt: '2016-04-10'
+                },
+
+                
+            }
+            
+        }
+    }
+
+
+</script>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,17 +1,30 @@
 <template>
   <div>
-    <NuxtTutorial />
+    <div style="border: 1px solid #fff;">
+      <NuxtTutorial />
+    </div>
+    <br />
+    <br />
+    <br />
+
+    <div style="border: 1px solid #fff;">
+      <ForLoop />
+
+    </div>
+    
   </div>
 
 </template>
 
 <script>
 import NuxtTutorial from '@/components/Tutorial';
+import ForLoop from '@/components/ForLoop'
 
 export default {
   name: 'IndexPage',
   components:{
     NuxtTutorial,
+    ForLoop,
   } 
 }
 </script>
